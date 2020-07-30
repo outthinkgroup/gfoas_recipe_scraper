384fe943934d56cbcd334ba310ac54272c5ade82
Joseph Hinson <j@outthink.cc>
2020-07-30 12:46:13 -0400

    removed the html_to_array function since we're not using it anymore

--- classes/class-gfoas-scraper.php
+++ classes/class-gfoas-scraper.php
@@ -209,29 +209,6 @@ class Recipe {
     return $returned_category_ids;
   }
 
-  private function html_to_array($html){
-    // filters the_content()
-    if(strpos($html, '</p>')){
-
-      $split_by_closing_tag = explode('</p>', $html);
-      $list = [];
-      foreach($split_by_closing_tag as $item){
-
-        $list_item = str_replace('<p>','',$item);
-        
-        //removes '' left over from the last '</p>'
-        if(strlen($list_item) > 1){
-          $list[] = $list_item;
-        }
-      }
-      
-      return $list;
-      
-    } else {
-      return $html;
-    }
-  }
-
   private function clean_up_steps($steps){
     $cleaned_steps=[];
     
